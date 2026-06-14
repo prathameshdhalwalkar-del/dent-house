53760f0318f6b3771f7d1d9dd679407fb9b75430
prathameshdhalwalkar-del <prathameshdhalwalkar-del@users.noreply.github.com>
2026-06-14 17:35:01 +0530

    Fix: hash scroll on cross-page navigation

--- src/components/HashScroll.tsx
+++ src/components/HashScroll.tsx
@@ -1,10 +1,23 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 export default function HashScroll() {
+  const [hash, setHash] = useState('')
+  const pathname = usePathname()
+
   useEffect(() => {
-    const hash = window.location.hash
+    setHash(window.location.hash)
+  }, [pathname])
+
+  useEffect(() => {
+    const onHashChange = () => setHash(window.location.hash)
+    window.addEventListener('hashchange', onHashChange)
+    return () => window.removeEventListener('hashchange', onHashChange)
+  }, [])
+
+  useEffect(() => {
     if (hash) {
       const retry = setInterval(() => {
         const el = document.querySelector(hash)
@@ -15,7 +28,7 @@ export default function HashScroll() {
       }, 100)
       setTimeout(() => clearInterval(retry), 5000)
     }
-  }, [])
+  }, [hash])
 
   return null
 }
